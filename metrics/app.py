95c8305901b210a9dd16acf2944c59329960e68b
Luiz <phelip@thenets.org>
2018-10-03 23:32:17 -0300

    Network working

--- metrics/app.py
+++ metrics/app.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
-import psutil, statistics, json
+from flask import Flask, jsonify
+import psutil, statistics
 
 app = Flask(__name__)
 
@@ -19,9 +19,9 @@ def say_hello(name):
 @app.route("/metrics/cpu")
 def metrics_cpu():
     cpu = {}
-    cpu['per_cpu'] = psutil.cpu_percent(interval=1, percpu=True)._asdict()
+    cpu['per_cpu'] = psutil.cpu_percent(interval=1, percpu=True)
     cpu['total'] = statistics.median(cpu['per_cpu'])
-    return json.dumps(cpu)
+    return jsonify(cpu)
 
 
 @app.route("/metrics/ram")
@@ -29,18 +29,41 @@ def metrics_ram():
     memory = {}
     memory['virtual'] = psutil.virtual_memory()._asdict()
     memory['swap'] = psutil.swap_memory()._asdict()
-    return json.dumps(memory)
+    return jsonify(memory)
 
 
 @app.route("/metrics/disk")
 def metrics_disk():
-    pass
+    disks = {}
+    partitions = psutil.disk_partitions()
+    for partition in partitions:
+        if "tmpfs" in partition.device:
+            continue
+        disks[partition.device] = {
+            'device' : partition.device,
+            'usage' : psutil.disk_usage(partition.mountpoint)._asdict()
+        }
+    return jsonify(disks)
 
 
+def getNetworkStats (nothing):
+    network = {}
+    network['interfaces'] = psutil.net_io_counters(pernic=True)
+    for key, value in network['interfaces'].items():
+        network['interfaces'][key] = value._asdict()
+    # network['connections'] = []
+    # for connection in psutil.net_connections():
+    #     network['connections'].append(connection._asdict())
+    print("sonho")
+    import json
+    return json.loads(json.dumps(network))
 @app.route("/metrics/network")
 def metrics_network():
-    pass
-
+    from multiprocessing import Pool
+    p = Pool(processes=20)
+    data = p.map(getNetworkStats, [1])
+    p.close()
+    return jsonify(data[0])
 
 @app.route("/metrics/services")
 def metrics_services():
